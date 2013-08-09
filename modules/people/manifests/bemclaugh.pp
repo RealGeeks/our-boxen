c33f576a1e85fc2bc375671fa5c34b3bf14d2116
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-09 13:10:40 -1000

    created links to dotFiles

--- modules/people/manifests/bemclaugh.pp
+++ modules/people/manifests/bemclaugh.pp
@@ -1,11 +1,78 @@
 class people::bemclaugh {
     include projects::all
 
-    $home = "/Users/brian"
-    $dotfiles = "$home/.dotFiles"
+    $home       = "/Users/${::luser}"
+    $dotfiles   = "${home}/.dotFiles"
 
     repository { $dotfiles:
         source => 'bem@webfactional:/home/bem/repo/dotFiles',
         require => File[$home]
+    }
+
+    user { $id:
+      ensure => present,
+      shell  => "/bin/zsh",
+    }
+
+    file { "${home}/.gitconfig":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.gitconfig",
+        require => Repository[$dotfiles],
+    }
+
+    file { "${home}/.gitignore_global":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.gitignore_global",
+        require => Repository[$dotfiles],
+    }
+    file { "${home}/.profile":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.profile",
+        require => Repository[$dotfiles],
+    }
+
+    file { "${home}/.tmux.conf":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.tmux.conf",
+        require => Repository[$dotfiles],
+    }
+
+    file { "${home}/.vim":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.vim",
+        require => Repository[$dotfiles],
+    }
+
+    file { "${home}/.vimrc":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.vimrc",
+        require => Repository[$dotfiles],
+    }
+
+    file { "${home}/.aliases":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.aliases",
+        require => Repository[$dotfiles],
+    }
+    file { "${home}/.oh-my-zsh":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.oh-my-zsh",
+        require => Repository[$dotfiles],
+    }
+
+    file { "${home}/.zshrc":
+        ensure => 'link',
+        mode => '0644',
+        target => "${dotfiles}/.zshrc",
+        require => Repository[$dotfiles],
+        line => "source ${home}/.zshrc"
     }
 }
