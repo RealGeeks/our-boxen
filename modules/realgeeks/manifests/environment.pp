70beb5b9a945079f1bd53ade0e7172b30cbdbb33
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-15 16:10:05 -1000

    organized

--- modules/realgeeks/manifests/environment.pp
+++ modules/realgeeks/manifests/environment.pp
@@ -1,16 +1,13 @@
 class realgeeks::environment {
-  include osx::global::disable_key_press_and_hold
-  include osx::global::disable_remote_control_ir_receiver
-  include osx::software_update
-  class { 'osx::global::natural_mouse_scrolling': enabled => false }
-  class { 'osx::global::key_repeat_rate': rate => 2 }
-  class { 'osx::global::key_repeat_delay': delay => 10 }
-  include python
-  include vagrant
-  include zsh
-  include wget
-  include python
-  include mongodb
-  include virtualbox
-  include vagrant
+
+    notify { 'class realgeeks::environment declared': }
+    include osx::recovery_message { 'If this Mac is found, please call 808.261.0616': }
+
+    include osx::global::disable_key_press_and_hold
+    include osx::global::disable_remote_control_ir_receiver
+    class { 'osx::global::natural_mouse_scrolling': enabled => false }
+    class { 'osx::global::key_repeat_rate': rate => 2 }
+    class { 'osx::global::key_repeat_delay': delay => 10 }
+
+    include projects::all
 }
