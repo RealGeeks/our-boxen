677a4a7bf8eeac7342221c01bece772ab388f35a
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-15 15:45:26 -1000

    added rg projects, organized

--- modules/people/manifests/bemclaugh.pp
+++ modules/people/manifests/bemclaugh.pp
@@ -1,78 +1,15 @@
 class people::bemclaugh {
-    include projects::all
 
-    $home       = "/Users/${::luser}"
-    $dotfiles   = "${home}/.dotFiles"
+    notify { 'class people::bemclaugh declared': }
 
-    repository { $dotfiles:
-        source => 'bem@webfactional:/home/bem/repo/dotFiles',
-        require => File[$home]
-    }
+    include people::bemclaugh::apps
+    include people::bemclaugh::dotfiles
+    include people::bemclaugh::osx
+    include people::bemclaugh::sublime_text_2
 
-    user { $id:
-      ensure => present,
-      shell  => "/bin/zsh",
-    }
+    include projects::rg2
+    include projects::server_config
+    include projects::pulley
+    include projects::lm_interface
 
-    file { "${home}/.gitconfig":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.gitconfig",
-        require => Repository[$dotfiles],
-    }
-
-    file { "${home}/.gitignore_global":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.gitignore_global",
-        require => Repository[$dotfiles],
-    }
-    file { "${home}/.profile":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.profile",
-        require => Repository[$dotfiles],
-    }
-
-    file { "${home}/.tmux.conf":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.tmux.conf",
-        require => Repository[$dotfiles],
-    }
-
-    file { "${home}/.vim":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.vim",
-        require => Repository[$dotfiles],
-    }
-
-    file { "${home}/.vimrc":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.vimrc",
-        require => Repository[$dotfiles],
-    }
-
-    file { "${home}/.aliases":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.aliases",
-        require => Repository[$dotfiles],
-    }
-    file { "${home}/.oh-my-zsh":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.oh-my-zsh",
-        require => Repository[$dotfiles],
-    }
-
-    file { "${home}/.zshrc":
-        ensure => 'link',
-        mode => '0644',
-        target => "${dotfiles}/.zshrc",
-        require => Repository[$dotfiles],
-        line => "source ${home}/.zshrc"
-    }
 }
