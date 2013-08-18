a40f6fe058e504db5bf871847a60cb201905e719
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-18 13:11:58 -1000

    supressed sublime text

--- modules/people/manifests/bemclaugh.pp
+++ modules/people/manifests/bemclaugh.pp
@@ -5,7 +5,7 @@ class people::bemclaugh {
     include people::bemclaugh::apps
     include people::bemclaugh::dotfiles
     include people::bemclaugh::osx
-    include people::bemclaugh::sublime_text_2
+    #include people::bemclaugh::sublime_text_2
 
     include projects::rg2
     include projects::server_config
