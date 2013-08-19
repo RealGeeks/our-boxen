2a11b4f9449cbd9d9ac9d64d597387201bdf7496
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-18 14:16:41 -1000

    replaced luser with boxen_user, enabled symlinks, pointing to new repo

--- modules/people/manifests/bemclaugh/dotfiles.pp
+++ modules/people/manifests/bemclaugh/dotfiles.pp
@@ -2,74 +2,45 @@ class people::bemclaugh::dotfiles {
 
     notify { 'class people::bemclaugh::dotfiles declared': }
 
-    repository {
-        "dotfiles":
-        source => 'bem@webfactional:/home/bem/repo/dotFiles',
-        path   => "${::boxen_srcdir}/dotfiles"
-    }
+    $home       = "/Users/${::boxen_user}"
+    $dotfiles_dir = "${home}/git/dotfiles"
 
-    $home       = "/Users/${::luser}"
+    repository { $dotfiles_dir:
+        source => "${::github_user}/dotfiles"
+    }
 
     file { "${home}/.gitconfig":
         ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.gitconfig",
-        require => Repository["dotfiles"],
+        target => "${dotfiles_dir}/gitconfig",
+        require => Repository[$dotfiles_dir],
     }
 
     file { "${home}/.gitignore_global":
         ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.gitignore_global",
-        require => Repository["dotfiles"],
+        target => "${dotfiles_dir}/gitignore_global",
+        require => Repository[$dotfiles_dir],
     }
+
     file { "${home}/.profile":
         ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.profile",
-        require => Repository["dotfiles"],
-    }
-
-    file { "${home}/.tmux.conf":
-        ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.tmux.conf",
-        require => Repository["dotfiles"],
-    }
-
-    file { "${home}/.vim":
-        ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.vim",
-        require => Repository["dotfiles"],
-    }
-
-    file { "${home}/.vimrc":
-        ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.vimrc",
-        require => Repository["dotfiles"],
+        target => "${dotfiles_dir}/profile",
+        require => Repository[$dotfiles_dir],
     }
 
     file { "${home}/.aliases":
         ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.aliases",
-        require => Repository["dotfiles"],
-    }
-
-    file { "${home}/.oh-my-zsh":
-        ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.oh-my-zsh",
-        require => Repository["dotfiles"],
+        target => "${dotfiles_dir}/aliases",
+        require => Repository[$dotfiles_dir],
     }
 
     file { "${home}/.zshrc":
         ensure => link,
-        mode => '0644',
-        target => "${::boxen_srcdir}/dotfiles/.zshrc",
-        require => Repository["dotfiles"],
-        #line => "source ${home}/.zshrc"
+        target => "${dotfiles_dir}/zshrc",
+        require => Repository[$dotfiles_dir],
     }
+
+    #file { "${home}/.oh-my-zsh":
+    #    ensure => link,
+    #    target => "${::boxen_srcdir}/oh-my-zsh/.oh-my-zsh",
+    #}
 }
