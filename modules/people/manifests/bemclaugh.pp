9ef6f35c75290078c2f94c70b12e26a2d7846342
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-18 15:27:12 -1000

    try sublime text again, include my_repo

--- modules/people/manifests/bemclaugh.pp
+++ modules/people/manifests/bemclaugh.pp
@@ -2,10 +2,11 @@ class people::bemclaugh {
 
     notify { 'class people::bemclaugh declared': }
 
+    include people::bemclaugh::my_repo
     include people::bemclaugh::apps
     include people::bemclaugh::dotfiles
     include people::bemclaugh::osx
-    #include people::bemclaugh::sublime_text_2
+    include people::bemclaugh::sublime_text_2
 
     include projects::rg2
     include projects::server_config
