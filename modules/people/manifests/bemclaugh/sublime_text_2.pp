eb6db48f87ef890c3f8470dd4c4934b85baac00e
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-18 14:17:25 -1000

    -mreplaced luser with boxen_user, added dotfile repo info here

--- modules/people/manifests/bemclaugh/sublime_text_2.pp
+++ modules/people/manifests/bemclaugh/sublime_text_2.pp
@@ -2,7 +2,13 @@ class people::bemclaugh::sublime_text_2 {
 
     notify { 'class people::bemclaugh::sublime_text_2 declared': }
 
-    $base = "/Users/${::luser}/Library/Application Support"
+    $home         = "/Users/${::boxen_user}"
+    $dotfiles_dir = "${home}/git/dotfiles"
+    $base         = "${home}/Library/Application Support"
+
+    repository { $dotfiles_dir:
+        source => "${::github_user}/dotfiles"
+    }
 
     exec { 'Idempotent creation of User preferences directory':
         command => "mkdir -p '${base}/Sublime Text 2/Packages/User'"
@@ -10,8 +16,8 @@ class people::bemclaugh::sublime_text_2 {
 
     file { "${base}/Sublime Text 2/Packages/User/Preferences.sublime-settings":
         ensure  => link,
-        target  => "${::boxen_srcdir}/dotfiles/.sublime/Preferences.sublime-settings",
-        require => Repository["dotfiles"],
+        target  => "${dotfiles_dir}/.sublime/Preferences.sublime-settings",
+        require => Repository[$dotfiles_dir],
     }
 
     sublime_text_2::package { 'SublimeLinter':
