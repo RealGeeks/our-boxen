fc1302a1a21a6318e37c8d5a54a10201d908e2a8
Brian McLaughlin <bemclaugh@gmail.com>
2013-08-25 11:49:21 -1000

    debug notices

--- modules/people/manifests/bemclaugh.pp
+++ modules/people/manifests/bemclaugh.pp
@@ -1,6 +1,10 @@
 class people::bemclaugh {
 
     notify { 'class people::bemclaugh declared': }
+    notify { "github user is ${::github_name}": }
+    notify { "github email is ${::github_email}": }
+    notify { "github login is ${::github_login}": }
+    notify { "boxen user is ${::boxen_user}": }
 
     include people::bemclaugh::my_repo
     include people::bemclaugh::apps
