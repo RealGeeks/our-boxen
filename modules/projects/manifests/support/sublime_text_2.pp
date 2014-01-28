4c3ed88ea9cac89cdf1cb0174938580f9ab736ce
Brian McLaughlin <bemclaugh@gmail.com>
2014-01-28 10:46:17 -1000

    added support project for support staff

---
+++ modules/projects/manifests/support/sublime_text_2.pp
@@ -0,0 +1,18 @@
+class projects::support::sublime_text_2 {
+
+    notify { 'class projects::support::sublime_text_2 declared': }
+
+    include sublime_text_2
+
+    $home         = /Users/${::boxen_user}
+    $base         = "${home}/Library/Application Support"
+    $sublime_dir  = "${base}/Sublime Text 2/Packages/User"
+
+    file { $sublime_dir:
+        ensure => directory,
+    }
+
+    sublime_text_2::package { 'SublimeLinter':
+        source => 'SublimeLinter/SublimeLinter',
+    }
+
