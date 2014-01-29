e10ba4ae8d9cf624cae7b33a7deb9fdeb306f634
Brian McLaughlin <bemclaugh@gmail.com>
2014-01-29 10:58:27 -1000

    added manifest for paul

---
+++ modules/people/manifests/Pezzzo1234.pp
@@ -0,0 +1,14 @@
+class people:Pezzo1234 {
+
+    $home = "/Users/${::boxen_user}"
+    $git_dir  = "${home}/git"
+
+    notify { 'class people::Pezzo1234 declared': }
+    notify { "github user is ${::github_name}": }
+    notify { "github email is ${::github_email}": }
+    notify { "github login is ${::github_login}": }
+    notify { "boxen user is ${::boxen_user}": }
+
+    include projects::realgeeks::support
+
+}
