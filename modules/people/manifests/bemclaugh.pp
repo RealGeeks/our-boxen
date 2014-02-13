49e5e4ebeb58e7f33c9c864d97171dd10e0980ba
Brian McLaughlin <bemclaugh@gmail.com>
2014-02-12 15:06:37 -1000

    testing support project

--- modules/people/manifests/bemclaugh.pp
+++ modules/people/manifests/bemclaugh.pp
@@ -2,7 +2,6 @@ class people::bemclaugh {
 
     $home     = "/Users/${::boxen_user}"
     $git_dir  = "${home}/git"
-    $dotfiles = "${git_dir}/dotfiles"
 
     notify { 'class people::bemclaugh declared': }
     notify { "github user is ${::github_name}": }
@@ -10,16 +9,6 @@ class people::bemclaugh {
     notify { "github login is ${::github_login}": }
     notify { "boxen user is ${::boxen_user}": }
 
-    include people::bemclaugh::my_repo
-    include people::bemclaugh::apps
-    include people::bemclaugh::dotfiles
-    include people::bemclaugh::osx
-    include people::bemclaugh::sublime_text_2
-
-    include projects::realgeeks::environment
-    include projects::realgeeks::rg2
-    include projects::realgeeks::server_config
-    include projects::realgeeks::pulley
-    include projects::realgeeks::lm_interface
+    include projects::realgeeks::support
 
 }
